c8ba498de9c580474487fc716ddd98df4d5ee158
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-03-04 12:50:24 +0100

    add comments && likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :likes_counter
+
+  private
+
   def likes_counter
-    post.increments!(:likes_counter)
+    post.increment!(:likes_counter)
   end
 end
