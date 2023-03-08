e3f3da8c54574ed68e45e374a6beb4d421ad24b7
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-03-08 22:31:27 +0100

    fix n +1 problem && write integration test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create(name: "Ifeanyi", photo: "https://avatars.githubusercontent.com/u/105528809?v=4", bio: "Software engineer")
+laura  = User.create(name: "laura", photo: "https://avatars.githubusercontent.com/u/105006208?v=4", bio:"software engineer")
+second_user = User.create(name: "joseph", photo: "https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", bio: "graphics designer")
+first_post = Post.create(author: user1, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: user1, title: 'hi', text: 'This is my second post')
+third_post = Post.create(author: user1, title: 'okay', text: 'This is my third post')
+fourth_post = Post.create(author: user1, title: 'yah', text: 'This is my fourth post')
+fifth_post = Post.create(author: user1, title: 'bro', text: 'This is my fifth post')
+Comment.create(post: first_post, author: user1, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: user1, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: user1, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: user1, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: user1, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: user1, text: 'Hi Tom!' )
---
+++ spec/views/posts/posts_index_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :system do
+  before :each do
+    @user = User.create(name: 'Simon', photo: 'https://randomuser.me/api/portraits/men/1.jpg', bio: 'Teacher in South Africa')
+    @first_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+    @comment = Comment.create(post: @first_post, author: @user, text: 'Hi Tom!' )
+  end
+  describe "Post page" do
+    it "shows the user's profile" do
+      visit "/users/#{@user.id}/posts"
+      expect(page).to have_css("img[src*='https://randomuser.me/api/portraits/men/1.jpg']")
+    end
+    it "shows the user's username" do
+      user_name = Post.find_by(author: @user.name)
+      visit "/users/#{@user.id}/posts"
+      expect(page).to have_content(user_name)
+    end
+    it "shows the number of post" do
+      user_name = Post.find_by(author: @user.name)
+      visit "/users/#{@user.id}/posts"
+      expect(page).to have_content(@user.post_counter)
+    end
+    it "shows the title of the post" do
+      visit "/users/#{@user.id}/posts"
+      expect(page).to have_content("Hello")
+    end
+    it "shows the body of the post" do
+      visit "/users/#{@user.id}/posts"
+      expect(page).to have_content("This is my first post")
+    end
+    it "shows the first comment of the post" do
+      visit "/users/#{@user.id}/posts"
+      expect(page).to have_content(@comment.text)
+    end
+    it "shows the number of likes && comments of the post" do
+      visit "/users/#{@user.id}/posts"
+      user_post = @user.posts.find_by(id: @first_post.id)
+      expect(page).to have_content(user_post.comments.count)
+      expect(page).to have_content(user_post.likes.count)
+    end
+    it "redirects me to post show page when I click on a post" do
+      link = @first_post.title
+      visit "/users/#{@user.id}/posts"
+      click_link link
+      expect(page).to have_current_path(user_post_path(@user.id, @first_post.id))
+    end
+  end
+end
