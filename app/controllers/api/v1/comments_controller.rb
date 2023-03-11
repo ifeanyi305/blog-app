caa7732813865b7cbc9927813dea7e685337db8f
suzana marsela <66145033+Suzi216@users.noreply.github.com>
2023-03-11 09:17:31 +0100

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -21,6 +21,10 @@ class Api::V1::CommentsController < ApplicationController
 
   private
 
+def set_post
+    @post = Post.find(params[:post_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:text)
   end
